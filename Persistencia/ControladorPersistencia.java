2093cbba40d96a49ed0cdb85092895f1a6532a2b
Carlos Aires <carlos.ae19@gmail.com>
2018-06-07 14:24:36 +0200

    Tot Persistencia

---
+++ Persistencia/ControladorPersistencia.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Persistencia;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+/**
+ *
+ * @author carlosaires
+ */
+public class ControladorPersistencia {
+    PersistenciaJugador pj = new PersistenciaJugador();
+    
+    
+    
+    public ControladorPersistencia() {
+        
+    }
+    
+    public void guardarJugador (String jug, String usr) throws FileNotFoundException {
+        
+        pj.guardarJugador(jug, usr);
+    }
+    
+    public String cargarJugador (String usr) throws FileNotFoundException, IOException, ClassNotFoundException {
+        return pj.cargarJugador(usr);
+    }
+    
+}
